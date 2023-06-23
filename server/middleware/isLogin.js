cabee973fc22d997567a3e7cffefc5bd5cbc0046
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-06-23 22:03:38 +0530

    create isLogin middleware

--- server/middleware/isLogin.js
+++ server/middleware/isLogin.js
@@ -1,21 +1,19 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+require("dotenv").config()
+const jwt = require("jsonwebtoken")
+const { User } = require("../models/user")
 
-const { JWT_SECRET } = process.env.JWT_SECRET
-const User = mongoose.model.User
+const JWT_SECRET = process.env.JWT_SECRET
 
 const isLogin = (req, res, next) => {
     const { authorization } = req.headers
     if (!authorization) {
-        return res.status(401).json({ error: "You must be logged in" })
+        return res.status(422).json({ error: "You must be logged in" })
     }
     const token = authorization.replace("Bearer ", "")
     jwt.verify(token, JWT_SECRET, (err, payload) => {
         if (err) {
-            return res.status(401).json({ error: "You must be logged in" })
+            return res.status(422).json({ error: "You must be logged in" })
         }
-        console.log(payload)
         const { _id } = payload
         User.findById(_id)
             .then(userData => {
