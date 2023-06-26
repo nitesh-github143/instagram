1b293667a0a0df23bb69c25ba7b80c83b9e04957
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-06-26 21:16:50 +0530

    create user profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react';
+import NetworkContext from '../context/NetworkContext';
+import UserContext from '../context/UserContext';
 
 const Profile = () => {
+    const networkUrl = useContext(NetworkContext)
+    const { state, dispatch } = useContext(UserContext)
+
+    const [mypic, setMyPics] = useState([])
+    useEffect(() => {
+        fetch(`${networkUrl}/mypost`, {
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            }
+        })
+            .then(res => res.json())
+            .then(result => {
+                setMyPics(result.mypost)
+            })
+    }, [])
     return (
         <div className="flex flex-col items-center">
             <div className=" w-2/3 bg-white shadow-md rounded-lg p-4 mb-4">
@@ -11,7 +28,7 @@ const Profile = () => {
                         alt="Profile"
                     />
                     <div>
-                        <h2 className="text-xl font-bold">John Doe</h2>
+                        <h2 className="text-xl font-bold">{state ? state.name : "Loading"}</h2>
                     </div>
                 </div>
                 <div className="flex justify-between mb-4">
@@ -24,45 +41,27 @@ const Profile = () => {
                         <span className="text-gray-600">Following</span>
                     </div>
                     <div className="flex flex-col items-center">
-                        <span className="font-bold">100</span>
+                        <span className="font-bold">{mypic.length}</span>
                         <span className="text-gray-600">Posts</span>
                     </div>
                 </div>
             </div>
             <div className="w-full bg-white shadow-md rounded-lg p-4">
-                <div className="flex justify-between mb-4">
-                    <img
-                        className="w-1/3 rounded-lg mr-2"
-                        src="https://placekitten.com/400/400"
-                        alt="Post"
-                    />
-                    <img
-                        className="w-1/3 rounded-lg mx-2"
-                        src="https://placekitten.com/400/400"
-                        alt="Post"
-                    />
-                    <img
-                        className="w-1/3 rounded-lg ml-2"
-                        src="https://placekitten.com/400/400"
-                        alt="Post"
-                    />
-                </div>
-                <div className="flex justify-between">
-                    <img
-                        className="w-1/3 rounded-lg mr-2"
-                        src="https://placekitten.com/400/400"
-                        alt="Post"
-                    />
-                    <img
-                        className="w-1/3 rounded-lg mx-2"
-                        src="https://placekitten.com/400/400"
-                        alt="Post"
-                    />
-                    <img
-                        className="w-1/3 rounded-lg ml-2"
-                        src="https://placekitten.com/400/400"
-                        alt="Post"
-                    />
+                <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 gap-4">
+                    {
+                        mypic.map(item => {
+                            return (
+
+                                <img
+                                    key={item._id}
+                                    className="w-full h-full object-cover rounded-lg"
+                                    src={item.photo}
+                                    alt={item.title}
+                                />
+
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
