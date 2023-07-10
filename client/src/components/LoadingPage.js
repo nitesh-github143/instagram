43f01398df928f714b6721b2c5bca2c2bb0c8002
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-07-10 22:23:17 +0530

    add loading animation

---
+++ client/src/components/LoadingPage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+const LoadingPage = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+            </div>
+        );
+    }
+
+    return <div>Your content goes here...</div>;
+};
+
+export default LoadingPage;
