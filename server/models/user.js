c9509b3a45193cf9e252e34061a1880809df4d94
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-07-10 22:26:34 +0530

    add pic model

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
+const { ObjectId } = mongoose.Schema.Types
 
 const userSchema = new Schema({
     name: {
@@ -13,7 +14,23 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    pic: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6pBSU_b8j6bJgsAZmyFaXtzC5_FUnYMIB1yzwiA_82SwFwPdv&s"
+    },
+    followers: [
+        {
+            type: ObjectId,
+            ref: "User"
+        }
+    ],
+    following: [
+        {
+            type: ObjectId,
+            ref: "User"
+        }
+    ]
 })
 
 exports.User = mongoose.model("User", userSchema)
