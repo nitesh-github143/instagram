17698d1ae6cb0bf7fd0fc8736cbfdefb6e208ba1
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-07-14 10:53:21 +0530

    refactor

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,130 +1,140 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import NetworkContext from '../context/NetworkContext'
-import UserContext from '../context/UserContext';
-import AnimatedPage from '../components/AnimatedPage';
+import NetworkContext from "../context/NetworkContext";
+import UserContext from "../context/UserContext";
+import AnimatedPage from "../components/AnimatedPage";
 
-
-import LoadingPage from "../components/LoadingPage"
+import LoadingPage from "../components/LoadingPage";
 
 const Login = () => {
-    const { state, dispatch } = useContext(UserContext)
-    const networkUrl = useContext(NetworkContext)
-    const navigate = useNavigate()
+  const { state, dispatch } = useContext(UserContext);
+  const networkUrl = useContext(NetworkContext);
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [isProcessing, setIsProcessing] = useState(false)
-    const [errorToast, setErrorToast] = useState(null)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [errorToast, setErrorToast] = useState(null);
 
-    const postData = (e) => {
-        e.preventDefault()
+  const postData = (e) => {
+    setIsProcessing(true);
+    e.preventDefault();
 
-        fetch(`${networkUrl}/login`, {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email,
-                password
-            })
-        }).then(res => res.json())
-            .then(data => {
-                if (data.error) {
-                    setErrorToast(data.error);
-                }
-                else {
-                    setIsProcessing(true)
-                    localStorage.setItem("jwt", data.token)
-                    localStorage.setItem("user", JSON.stringify(data.user))
-                    dispatch({ type: "USER", payload: data.user })
-                    setIsProcessing(false)
-                    navigate('/')
-                }
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
+    fetch(`${networkUrl}/login`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          setIsProcessing(false);
+          setErrorToast(data.error);
+        } else {
+          setIsProcessing(true);
+          localStorage.setItem("jwt", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+          dispatch({ type: "USER", payload: data.user });
+          setIsProcessing(false);
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    if (isProcessing) {
-        return <LoadingPage />;
-    }
+  if (isProcessing) {
+    return <LoadingPage />;
+  }
 
-    return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Login to your account
-                </h2>
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Login to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+              />
             </div>
+          </div>
 
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6">
-
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                            Email address
-                        </label>
-                        <div className="mt-2">
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                autoComplete="email"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <div className="flex items-center justify-between">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
-                            </label>
-                            <div className="text-sm">
-                                <a href="/" className="font-semibold text-indigo-600 hover:text-indigo-500 ">
-                                    Forgot password?
-                                </a>
-                            </div>
-                        </div>
-                        <div className="mt-2">
-                            <input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                autoComplete="current-password"
-                                required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault()
-                                postData(e)
-                            }}
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Login
-                        </button>
-                    </div>
-                </form>
-                {errorToast && (
-                    <AnimatedPage message={errorToast} type="error" />
-                )}
-                <p className="mt-10 text-center text-sm text-gray-500">
-                    <Link to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                        Don't have an account ?
-                    </Link>
-                </p>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="/"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500 "
+                >
+                  Forgot password?
+                </a>
+              </div>
             </div>
-        </div>
-    )
-}
+            <div className="mt-2">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+              />
+            </div>
+          </div>
 
-export default Login
+          <div>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                postData(e);
+              }}
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+        {errorToast && <AnimatedPage message={errorToast} type="error" />}
+        <p className="mt-10 text-center text-sm text-gray-500">
+          <Link
+            to="/signup"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Don't have an account ?
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
